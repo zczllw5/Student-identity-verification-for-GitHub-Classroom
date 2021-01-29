41967e1bc0cd87fe724830cd550a9732b4b98489
Lian <lian.wang.work@gmail.com>
2021-01-29 14:11:14 +0800

    weeklyUpdate

--- GitHubAuth.py
+++ GitHubAuth.py
@@ -1,7 +1,18 @@
 import requests 
-#user = "zczllw5"
-#password = "Wl911023"
+import pandas as pd
+import re
+
+# match UCL email address by and .xlsx filr
+
+f = open('group_export.txt', "r")
+t = f.read()
+
+emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", t)
+print (emails)
+
+f.close
+
 r = requests.get('https://api.github.com/users/zczllw5')
-#print(user)
-#print(password)
-print (r)
+print(r.text)
+
+#''['[]]
